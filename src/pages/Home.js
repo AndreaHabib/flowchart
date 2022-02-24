8748d82539fde5f08e4642bae12f9e484498366b
AndreaHabib <andrea.atef@yahoo.com>
2022-02-24 11:01:52 -0500

    FEAT: working on flowchart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Stack, Alert, Typography, List, ListItem, Box } from "@mui/material";
+import { Stack, Alert, Typography, Button, Box } from "@mui/material";
 
 export default function Home() {
   return (
@@ -15,6 +15,54 @@ export default function Home() {
           </Typography>
         </Alert>
         <Alert sx={{ textAlign: "left" }} variant="outlined" severity="info">
+          <Typography variant="h6">Step 1:</Typography>
+          <Typography mb={3} variant="body1">
+            Obtain your catalog year from DegreeWorks and return to this page.
+          </Typography>
+          <Button
+            variant="contained"
+            size="small"
+            rel="noreferrer noopener"
+            target="_blank"
+            href="https://degreeworks.cuny.edu/Dashboard_si/dashboard"
+            component="a"
+          >
+            DegreeWorks
+          </Button>
+        </Alert>
+        <Alert sx={{ textAlign: "left" }} variant="outlined" severity="info">
+          <Typography variant="h6">Step 2:</Typography>
+          <Typography mb={3} variant="body1">
+            Select your prerequisite flowchart from the menu below based on your
+            catalog year obtained in step 1.
+          </Typography>
+        </Alert>
+        <Alert variant="outlined" severity="info">
+          <Typography variant="h6">Where to find your catalog year:</Typography>
+          <Box
+            sx={{
+              display: { xs: "none", sm: "none", md: "block", lg: "block" },
+            }}
+          >
+            <img
+              style={{ width: "80%", height: "auto" }}
+              src="https://res.cloudinary.com/andreahabib/image/upload/v1645115452/Screen_Shot_2022-02-17_at_11.17.59_AM_j0gxlb.png"
+              alt="degreeworks"
+            ></img>
+          </Box>
+          <Box
+            sx={{
+              display: { xs: "block", sm: "block", md: "none", lg: "none" },
+            }}
+          >
+            <img
+              style={{ width: "100%", height: "auto" }}
+              src="https://res.cloudinary.com/andreahabib/image/upload/v1645382846/Screen_Shot_2022-02-20_at_1.42.55_PM_oaidcl.png"
+              alt="degreeworks"
+            ></img>
+          </Box>
+        </Alert>
+        <Alert sx={{ textAlign: "left" }} variant="outlined" severity="info">
           <Typography variant="h6">What is a flowchart?</Typography>
           <Typography variant="body1">
             A flowchart is a visual representation of the prerequisites to help
@@ -28,7 +76,7 @@ export default function Home() {
             complete according to your CSC catalog year.
           </Typography>
         </Alert> */}
-        <Alert variant="outlined" severity="info">
+        {/* <Alert variant="outlined" severity="info">
           <Typography variant="h6">
             How to find your respective prerequisite flowchart:
           </Typography>
@@ -80,7 +128,7 @@ export default function Home() {
               </Typography>
             </ListItem>
           </List>
-        </Alert>
+        </Alert> */}
         <Alert variant="outlined" severity="error">
           The information is intended to inform and is not a replacement for a
           degree audit conducted with an academic advisor
